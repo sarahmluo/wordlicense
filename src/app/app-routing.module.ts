cd524532abaf9c4b191f0c89b278a66021edd65c
sarahmluo <sarah.m.luo@gmail.com>
2018-11-23 23:15:23 -0500

    Add play, instruction, about pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'play', loadChildren: './play/play.module#PlayPageModule' },
+  { path: 'instructions', loadChildren: './instructions/instructions.module#InstructionsPageModule' },
+  { path: 'about', loadChildren: './about/about.module#AboutPageModule' }
 ];
 
 @NgModule({
