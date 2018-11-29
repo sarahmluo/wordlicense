f00737a1025853ca0e09e9731ac5cb277f1e1ddb
sarahmluo <sarah.m.luo@gmail.com>
2018-11-29 16:43:17 -0500

    Style play page

--- src/app/play/license-plate/license-plate.component.ts
+++ src/app/play/license-plate/license-plate.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-license-plate',
@@ -9,14 +9,46 @@ export class LicensePlateComponent implements OnInit {
   constructor() { }
 
   /**
+   * Image div.
+   */
+  @ViewChild('licensePlate')
+  public imageDiv: ElementRef;
+
+  /**
    * Flag indicating if the game has started.
    */
   public hasStarted: boolean = false;
 
   /**
+   * Dynamic css for image div.
+   */
+  public imageCss: any = {};
+
+  /**
    * On Init.
    */
   public ngOnInit(): void {
+    this.calculateImageCss();
+  }
+
+  /**
+   * Calculate dynamic css properties for the license image div.
+   */
+  private calculateImageCss(): void {
+    let minHeight: string;
+    let paddingTop: string;
+    console.log(this.imageDiv);
+    const width: number = window.innerWidth;
+    console.log('width: ' + width);
+    // todo: move hardcoded values to a service
+    const height: number = Math.round((width * 300) / 600);
+
+    this.imageCss = {
+      height: height + 'px',
+      'font-size': Math.round(height/10) + 'px', 
+    }
+
+    console.log(minHeight);
   }
 
 }
