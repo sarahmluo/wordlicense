e495861c6afc3688638dc9d3e43f7dc0f892a6d3
sarahmluo <sarah.m.luo@gmail.com>
2021-04-16 11:00:28 -0400

    Update build file

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-npm install && ionic cordova platform add android && copy /y release-signing.properties platforms\android && npm run publish.android
+npm install && ionic cordova platform add android@9.0.0 && copy /y release-signing.properties platforms\android && npm run publish.android
